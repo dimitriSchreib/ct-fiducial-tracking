adbf1e2b987c8704fd02c9eb865b12d134d8d9b9
Derek Chen <dec001@ucsd.edu>
2022-09-21 15:03:40 -0700

    Ready for Crane Open loop Testing

--- ct_tracking_library/ct_processing_functions.py
+++ ct_tracking_library/ct_processing_functions.py
@@ -77,21 +77,45 @@ def convert_mha_to_mesh(mha_file='temp_mesh.mha', output_mesh_file = 'temp_mesh.
     '''
     # convert from SimpleITK to Numpy
     image_3D = sitk.ReadImage('temp_mesh.mha')
-
     spacing_array = np.array([image_3D.GetSpacing()[2],image_3D.GetSpacing()[1],image_3D.GetSpacing()[0]])
     print("spacing: ",spacing_array)
     origin = np.array([image_3D.GetOrigin()[2], image_3D.GetOrigin()[1], image_3D.GetOrigin()[0]])
+    #KEY POINT: http://simpleitk.org/SimpleITK-Notebooks/01_Image_Basics.html
+    #(Z,Y,X) sapcing for numpy. Adding in -1 to fix Z inversion?
+    #(X,Y,Z) spacing for simpleitk
+    #spacing_array[0]*=-1
+    #potential reason why to flip axis of numpy array: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6168008/pdf/nihms-987853.pdf
+    #https://simpleitk.readthedocs.io/en/v1.2.4/Documentation/docs/source/fundamentalConcepts.html
+    #function: https://simpleitk.readthedocs.io/en/v1.2.4/Documentation/docs/source/fundamentalConcepts.html
+    #TransformIndexToPhysicalPoint
+    #defining the above function: http://insightsoftwareconsortium.github.io/SimpleITK-Notebooks/Python_html/21_Transforms_and_Resampling.html
+    print('image shape in sitk (x,y,z): width {} then height {} then depth {}'.format(image_3D.GetWidth(), image_3D.GetHeight(), image_3D.GetDepth()))
     print(" origin: ", origin)
     # #run marching cubes
     # # convert mha into npdrarray with int type
     ellip_double = sitk.GetArrayFromImage(image_3D)
+    print('numpy version shape: (z,y,x) {}'.format(ellip_double.shape))
+    extreme_points = [image_3D.TransformIndexToPhysicalPoint((0,0,0)), 
+                  #image_3D.TransformIndexToPhysicalPoint((image_3D.GetWidth(),0)),
+                  image_3D.TransformIndexToPhysicalPoint((image_3D.GetWidth(),image_3D.GetHeight(),image_3D.GetDepth()))]
+    extreme_pixels = [image_3D.GetPixel((256,256,0)), 
+                  #image_3D.TransformIndexToPhysicalPoint((image_3D.GetWidth(),0)),
+                  image_3D.GetPixel((256,256,image_3D.GetDepth()-1))]
+                  #image_3D.TransformIndexToPhysicalPoint((0,image_3D.GetHeight(),0))]
+    print('locations of different pixels in image {}'.format(extreme_points))
+    print('there math https://discourse.itk.org/t/solved-transformindextophysicalpoint-manually/1031/10')
+    print('perhpas mcubes mirrors?')
+    print('extreme simple itk pixels: {}'.format(extreme_pixels))
+    extreme_pixels = [ellip_double[(0,256,256)], 
+                  #image_3D.TransformIndexToPhysicalPoint((image_3D.GetWidth(),0)),
+                  ellip_double[(image_3D.GetDepth()-1,256,256)]]
+        
+    print('extreme simple itk pixels: {}'.format(extreme_pixels)) 
     ellip_double = ellip_double.astype(dtype='i2')
     # if crop_z:
     #     ellip_double = ellip_double[crop_z[0]:crop_z[1],:,:]
     #print("image_stack shape: ",ellip_double.shape)
-
     verts, triangles = mcubes.marching_cubes(ellip_double, threshold_value)
-
     # grid = pv.UniformGrid(
     #     dims=ellip_double.shape,
     #     spacing=(spacing_array[0],spacing_array[1],spacing_array[2]),
@@ -99,14 +123,19 @@ def convert_mha_to_mesh(mha_file='temp_mesh.mha', output_mesh_file = 'temp_mesh.
     #     )
     # mesh = grid.contour([1], ellip_double, method='marching_cubes')
     # mesh.plot(scalars=dist, smooth_shading=True, specular=5, cmap="plasma", show_scalar_bar=False)
-
     # verts, faces, normals, values = measure.marching_cubes(ellip_double, threshold_value)
-
     # #respace mesh vertices based on DICOM spacing
     verts = verts * spacing_array
-    
     # #offset verts due to origin
     verts += np.array([image_3D.GetOrigin()[2], image_3D.GetOrigin()[1], image_3D.GetOrigin()[0]])
+    #DIFFERENT IDEA! WE ARE IN Z Y X BUT NORMALLY THINGS ARE X Y Z. LETS SWAP.
+    print('verts shape: {}'.format(verts.shape))
+    print('verts type: {}'.format(type(verts)))
+    verts_reordered = np.zeros(np.array(verts).shape)
+    verts_reordered[:,0] = verts[:,2]
+    verts_reordered[:,1] = verts[:,1]
+    verts_reordered[:,2] = verts[:,0]
+    verts = copy.deepcopy(verts_reordered)
     mcubes.export_obj(verts, triangles, output_mesh_file)
     # #create mesh object and save to disk
     # cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
@@ -114,12 +143,11 @@ def convert_mha_to_mesh(mha_file='temp_mesh.mha', output_mesh_file = 'temp_mesh.
     #     for j in range(3):
     #         cube.vectors[i][j] = verts[f[j],:]
     # cube.save(output_mesh_file)
-
     if debug:
         saved_mesh_o3d = o3d.io.read_triangle_mesh(output_mesh_file)
         saved_mesh_o3d.compute_vertex_normals()
         print("Displaying segmented mesh")
-        o3d.visualization.draw_geometries([saved_mesh_o3d])
+        o3d.visualization.draw_geometries([saved_mesh_o3d])   
 
 def load_and_remesh(input_mesh_path):
     """ Load a mesh from disk and remesh to fix connectivity issues.
